75f5352e9c29699a88b02544e1b96abf623d4851
Simon Vieille <simon@deblan.fr>
2015-06-01 11:02:30 +0200

    function name

--- src/SftpClient.php
+++ src/SftpClient.php
@@ -448,29 +448,6 @@ class SftpClient
     }
 
     /**
-     * Attempts to remove a file using SFTP protocol
-     *
-     * @param string $remoteFilePath Remote file path. File must exist.
-     * @return self
-     * @throws Exception Invalid connection resource! due to use of validateSshResource.
-     * @throws Exception Unable to remove remote file!.
-     */
-    public function remove($remoteFilePath)
-    {
-        $this->validateSshResource();
-
-        if(ssh2_sftp_stat($this->getSftpResource(), $remoteFilePath) === false) {
-            throw new Exception("File does not exist or no permissions to read!");
-        }
-
-        if (ssh2_sftp_unlink($this->getSftpResource(), $remoteFilePath) === false) {
-            throw new Exception("Unable to remove remote file!");
-        }
-
-        return $this;
-    }
-
-    /**
      * Attempts to rename a file using SFTP protocol
      *
      * @param string $remoteFilePath Remote file path. File must exist.
@@ -478,7 +455,7 @@ class SftpClient
      * @throws Exception Invalid connection resource! due to use of validateSshResource.
      * @throws Exception Unable to rename remote file!.
      */
-    public function remove($remoteFilePath, $newRemoteFilePath)
+    public function rename($remoteFilePath, $newRemoteFilePath)
     {
         $this->validateSshResource();
 
